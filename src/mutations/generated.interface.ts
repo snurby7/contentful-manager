cd5b9de18ff184265b1d393ae19c447ecf033cab
Ryan Bruns <ryan.bruns7@gmail.com>
2019-10-01 15:00:07 -0500

    remove unused interface

--- src/mutations/generated.interface.ts
+++ src/mutations/generated.interface.ts
@@ -1,55 +1,10 @@
 // This was generated from the response of the export
 export interface ContentfulExport {
   contentTypes: any[];
-  entries?: EntriesEntity[] | null;
-  assets?: (null)[] | null;
+  entries?: any[];
+  assets?: any[];
   locales: any[];
   webhooks: any[];
   roles: any[];
   editorInterfaces: any[];
-}
-export interface EntriesEntity {
-  sys: Sys;
-  fields: Fields;
-}
-export interface Sys {
-  space: SpaceOrEnvironmentOrCreatedByOrUpdatedByOrPublishedByOrContentTypeOrEnUSOrEnUSEntity;
-  id: string | null;
-  type: string;
-  createdAt: string;
-  updatedAt: string;
-  environment: SpaceOrEnvironmentOrCreatedByOrUpdatedByOrPublishedByOrContentTypeOrEnUSOrEnUSEntity;
-  publishedVersion: number;
-  publishedAt: string;
-  firstPublishedAt: string;
-  createdBy: SpaceOrEnvironmentOrCreatedByOrUpdatedByOrPublishedByOrContentTypeOrEnUSOrEnUSEntity;
-  updatedBy: SpaceOrEnvironmentOrCreatedByOrUpdatedByOrPublishedByOrContentTypeOrEnUSOrEnUSEntity;
-  publishedCounter: number;
-  version: number;
-  publishedBy: SpaceOrEnvironmentOrCreatedByOrUpdatedByOrPublishedByOrContentTypeOrEnUSOrEnUSEntity;
-  contentType: SpaceOrEnvironmentOrCreatedByOrUpdatedByOrPublishedByOrContentTypeOrEnUSOrEnUSEntity;
-}
-export interface SpaceOrEnvironmentOrCreatedByOrUpdatedByOrPublishedByOrContentTypeOrEnUSOrEnUSEntity {
-  sys: Sys1;
-}
-export interface Sys1 {
-  type: string;
-  linkType: string;
-  id: string;
-}
-export interface Fields {
-  name: Name;
-  tag: Tag;
-  steps: Steps;
-}
-export interface Name {
-  ['en-US']: string;
-}
-export interface Tag {
-  ['en-US']: SpaceOrEnvironmentOrCreatedByOrUpdatedByOrPublishedByOrContentTypeOrEnUSOrEnUSEntity;
-}
-export interface Steps {
-  ['en-US']?:
-    | (SpaceOrEnvironmentOrCreatedByOrUpdatedByOrPublishedByOrContentTypeOrEnUSOrEnUSEntity)[]
-    | null;
 }
